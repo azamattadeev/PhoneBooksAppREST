ad5a4498707299a44bb1cecb96a285f36264916c
Azamat Tadeev <azamat.tadeev@mail.ru>
2019-12-07 12:03:01 +0300

    App was generated with Spring Initializr

---
+++ src/main/java/phonebooks/PhonebooksApplication.java
@@ -0,0 +1,13 @@
+package phonebooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonebooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/phonebooks/PhonebooksApplicationTests.java
@@ -0,0 +1,13 @@
+package phonebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonebooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
